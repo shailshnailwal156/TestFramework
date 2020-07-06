19bbf9ead20ce03f5d8afc376b26ae809c23c7f0
Shailsh Nailwal <shailsh.nailwal@sierradev.com>
2020-07-06 13:54:06 +0530

    test commit

---
+++ TestFramework/MyLable.swift
@@ -0,0 +1,100 @@
+//
+//  MyLable.swift
+//  TestFramework
+//
+//  Created by Shailsh Nailwal on 06/07/20.
+//  Copyright © 2020 Shailsh Nailwal. All rights reserved.
+//
+
+import UIKit
+
+
+public enum SootheFonts: Int {
+    case bold //0
+    case book // 1
+    case medium // 2
+    case regular //3
+    case light //4
+    case system //5
+    case tabBarButtonItem //6
+    case contextMessage //7
+
+    public init() {
+      self = .regular
+    }
+
+    public init(value: Int) {
+      var result: SootheFonts = SootheFonts()
+        switch value {
+        case SootheFonts.book.rawValue:
+            result = SootheFonts.book
+        case SootheFonts.medium.rawValue:
+            result = SootheFonts.medium
+        case SootheFonts.bold.rawValue:
+            result = SootheFonts.bold
+        case SootheFonts.regular.rawValue:
+            result = SootheFonts.regular
+        case SootheFonts.light.rawValue:
+            result = SootheFonts.light
+        case SootheFonts.system.rawValue:
+            result = SootheFonts.system
+        case SootheFonts.tabBarButtonItem.rawValue:
+            result = SootheFonts.system
+        case SootheFonts.contextMessage.rawValue:
+            result = SootheFonts.system
+        default:
+            result = SootheFonts.regular
+        }
+      self = result
+    }
+
+    public init(value: SootheFonts) {
+        self = SootheFonts(value: value.rawValue)
+    }
+
+    public func font(size: CGFloat) -> UIFont {
+        switch self {
+        case .book, .regular:
+            return UIFont(name: "GothamHTF-Book", size: size)!
+        case .medium:
+            return UIFont(name: "GothamHTF-Medium", size: size)!
+        case .bold:
+            return UIFont(name: "GothamHTF-Bold", size: size)!
+        case .light:
+            return UIFont(name: "GothamHTF-Light", size: size)!
+        case .system:
+            return UIFont.systemFont(ofSize: size)
+        case .tabBarButtonItem:
+            return UIFont.systemFont(ofSize: size)
+        case .contextMessage:
+            return UIFont.systemFont(ofSize: size)
+        }
+    }
+
+    /**
+     * Function will set the application font of any view of type UILabel, UITextField, UITextView and UIButton
+     */
+    public func setApplicationFont(toView view: UIView) {
+        if view is UILabel {
+            (view as! UILabel).font = font(size: (view as! UILabel).font.pointSize)
+        } else if view is UITextField {
+            (view as! UITextField).font = font(size: (view as! UITextField).font?.pointSize ?? 0)
+        } else if view is UITextView {
+            (view as! UITextView).font = font(size: (view as! UITextView).font?.pointSize ?? 0)
+        } else if view is UIButton {
+            (view as! UIButton).titleLabel?.font = font(size: (view as! UIButton).titleLabel?.font.pointSize ?? 0)
+        }
+    }
+}
+
+public class MyLable: UILabel {
+  var _font: SootheFonts = SootheFonts()
+  @IBInspectable
+  var customFont: Int = SootheFonts.regular.rawValue {
+     didSet {
+        self._font = SootheFonts(value: customFont) //.init(value: customFont)
+        self.font = _font.font(size: self.font.pointSize)
+       setNeedsDisplay()
+     }
+  }
+}
---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Shailsh Nailwal on 06/07/20.
+//  Copyright © 2020 Shailsh Nailwal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
