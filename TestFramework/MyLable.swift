3e9f1705c4e06ba64d46d90bb9c288166a29c492
Shailsh Nailwal <shailsh.nailwal@sierradev.com>
2020-07-06 21:11:30 +0530

    font load functions added

--- TestFramework/MyLable.swift
+++ TestFramework/MyLable.swift
@@ -8,6 +8,30 @@
 
 import UIKit
 
+extension UIFont {
+    private static func registerFont(withName name: String, fileExtension: String) {
+        let frameworkBundle = Bundle(for: MyLable.self)
+        let pathForResourceString = frameworkBundle.path(forResource: name, ofType: fileExtension)
+        let fontData = NSData(contentsOfFile: pathForResourceString!)
+        let dataProvider = CGDataProvider(data: fontData!)
+        let fontRef = CGFont(dataProvider!)
+        var errorRef: Unmanaged<CFError>? = nil
+
+        if (CTFontManagerRegisterGraphicsFont(fontRef!, &errorRef) == false) {
+            print("Error registering font")
+        }
+    }
+
+    public static func loadFonts() {
+        registerFont(withName: "GothamHTF-Light", fileExtension: "ttf")
+        registerFont(withName: "GothamHTF-Thin", fileExtension: "ttf")
+        registerFont(withName: "GothamHTF-Medium", fileExtension: "ttf")
+        registerFont(withName: "GothamHTF-Book", fileExtension: "ttf")
+        registerFont(withName: "GothamHTF-Bold", fileExtension: "ttf")
+        registerFont(withName: "GothamHTF-XLight", fileExtension: "ttf")
+    }
+}
+
 
 public enum SootheFonts: Int {
     case bold //0
